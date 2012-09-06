62dc7b0f1c476119793b81a52961d5a33eb1ee26
Andrew Garn <andrewgarn@gmail.com>
2012-09-06 09:26:30 +0100

    Removed unused code

--- Foundation/Categories/NSData+AGCategory.h
+++ Foundation/Categories/NSData+AGCategory.h
@@ -25,6 +25,8 @@
 //  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#import <Foundation/Foundation.h>
+
 /** A collection of category extensions for `NSData` */
 @interface NSData (AGCategory)
 
