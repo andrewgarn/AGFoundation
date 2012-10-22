823a17e409cc6cf7b7549f0ad7a4699ffb11f330
Andrew Garn <andrewgarn@gmail.com>
2012-10-22 23:35:20 +0100

    Added AGHungQueueDetector

--- Foundation/Extensions/AGHungQueueDetector.h
+++ Foundation/Extensions/AGHungQueueDetector.h
@@ -1,5 +1,5 @@
 //
-//  AGThreadHangDetector.h
+//  AGHungQueueDetector.h
 //  AGFoundation
 //
 //  Created by Andrew Garn on 22/10/2012.
@@ -7,7 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface AGThreadHangDetector : NSObject
+/** An `NSObject` subclass that will raise an exception when a targeted dispatch queue is blocked for more than a specified amount of time. */
+@interface AGHungQueueDetector : NSObject
+
+/** Initializes a new hung queue detector targeted at the main queue.
+ @return The receiver, initialized to observe the main queue.
+*/
+- (id)initWithMainQueue;
+
+/** Initializes a new hung queue detector targeted at the given dispatch queue.
+ @param targetQueue The dispatch queue to observe.
+ @return The receiver, initialized to observe the given queue.
+*/
+- (id)initWithTargetQueue:(dispatch_queue_t)targetQueue;
+
+/** Initializes a new hung queue detector targeted at the given dispatch queue.
+ @param targetQueue The dispatch queue to observe.
+ @param detectionInterval The interval that the given queue must be blocked for the detector to raise an exception.
+ @return The receiver, initialized to observe the given queue.
+*/
+- (id)initWithTargetQueue:(dispatch_queue_t)targetQueue interval:(CGFloat)detectionInterval;
+
+/** Ends the hung queue detector's observation of the targeted dispatch queue */ 
+- (void)stopObservingTargetedQueue;
 
 @end
--- Foundation/Extensions/AGHungQueueDetector.m
+++ Foundation/Extensions/AGHungQueueDetector.m
@@ -1,13 +1,86 @@
 //
-//  AGThreadHangDetector.m
+//  AGHungQueueDetector.m
 //  AGFoundation
 //
 //  Created by Andrew Garn on 22/10/2012.
 //  Copyright (c) 2012 Andrew Garn. All rights reserved.
 //
 
-#import "AGThreadHangDetector.h"
+#import "AGHungQueueDetector.h"
 
-@implementation AGThreadHangDetector
+static volatile BOOL AGHungQueueDetectorHeartbeat = YES;
+static CGFloat AGHungQueueDetectorDefaultDetectionInterval = 0.5;
+
+@interface AGHungQueueDetector ()
+@property (weak, nonatomic) NSTimer *intervalTimer;
+@property (assign, nonatomic) dispatch_queue_t targetQueue;
+@property (assign, nonatomic) double detectionInterval;
+@property (assign, nonatomic) BOOL cancelled;
+@end
+
+#pragma mark -
+
+@implementation AGHungQueueDetector
+
+#pragma mark -
+
+- (id)initWithMainQueue
+{
+    return [[AGHungQueueDetector alloc] initWithTargetQueue:dispatch_get_main_queue()];
+}
+
+- (id)initWithTargetQueue:(dispatch_queue_t)targetQueue
+{
+    return [[AGHungQueueDetector alloc] initWithTargetQueue:targetQueue interval:AGHungQueueDetectorDefaultDetectionInterval];
+}
+
+- (id)initWithTargetQueue:(dispatch_queue_t)targetQueue interval:(CGFloat)detectionInterval
+{
+    if ((self = [super init]))
+    {
+#ifdef DEBUG
+        NSAssert(targetQueue != nil, @"targetQueue must not be nil");
+        
+        _targetQueue = targetQueue;
+        _detectionInterval = detectionInterval;
+        
+        NSThread *detectionThread = [[NSThread alloc] initWithTarget:self selector:@selector(hungQueueDetectorMain) object:nil];
+        [detectionThread setName:@"AGHungQueueDetectorThread"];
+        [detectionThread start];
+#endif
+    }
+    return self;
+}
+
+- (void)stopObservingTargetedQueue
+{
+    self.cancelled = YES;
+    [self.intervalTimer invalidate];
+}
+
+#pragma mark -
+
+- (void)hungQueueDetectorTick
+{    
+    if (AGHungQueueDetectorHeartbeat == NO) {
+        [NSException raise:@"AGHungQueueDetectorException" format:@"Queue did not respond in %.1f seconds", self.detectionInterval];
+    }
+    
+    AGHungQueueDetectorHeartbeat = NO;
+    dispatch_async(self.targetQueue, ^{
+        AGHungQueueDetectorHeartbeat = YES;
+    });
+}
+
+- (void)hungQueueDetectorMain
+{
+    @autoreleasepool {
+        self.intervalTimer = [NSTimer scheduledTimerWithTimeInterval:self.detectionInterval target:self selector:@selector(hungQueueDetectorTick) userInfo:nil repeats:YES];
+    
+        do {
+            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+        } while (self.cancelled == NO);
+    }
+}
 
 @end
